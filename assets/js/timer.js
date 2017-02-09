8e01d142a904bf8fcb64c9d700685c5ea553add9
Fady <fadysamirsadek@live.com>
2017-02-09 18:11:06 +0200

    first commit

---
+++ assets/js/timer.js
@@ -0,0 +1,19 @@
+(function() {
+  var deadline = '2017-2-18';
+  function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+}
+document.getElementById('timer-content').innerHTML =getTimeRemaining(deadline).days ;
+console.log(getTimeRemaining(deadline).days);
+}());
